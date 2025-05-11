73e1441ef18b49419eaaba30b6412855338d1b22
Marc Zweiler <marc.zweiler@outlook.at>
2025-05-11 21:03:33 +0200

    Added a bunch of different color themes to nvim

--- init.lua
+++ init.lua
@@ -911,7 +911,34 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
+    end,
+  },
+
+  {
+    'thesimonho/kanagawa-paper.nvim',
+    priority = 1000,
+    config = function()
+      require('kanagawa-paper').setup {
+        style = 'ink',
+      }
+      vim.cmd.colorscheme 'kanagawa-paper'
+    end,
+  },
+
+  {
+    'sainnhe/edge',
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme 'edge'
+    end,
+  },
+
+  {
+    'sainnhe/gruvbox-material',
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme 'gruvbox-material'
     end,
   },
 
